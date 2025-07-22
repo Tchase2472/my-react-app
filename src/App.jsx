877e419027fb6c0b162eee4ca97b35ff9ee4dad0
Todd Chase <toddchase38@gmail.com>
2025-07-22 16:08:55 -0400

    Lists are overly complicated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import List from "./List.jsx"
 
+  const listItems = [
+                    
+                    {id : 1, name: "apple",   calories: 120},
+                    {id : 2, name: "pear",    calories: 23},
+                    {id : 3, name: "banana",  calories: 430},
+                    {id : 4, name: "coconut", calories: 100},
+                    {id : 5, name: "orange",  calories: 59},
+                ];      
+
 function App() {
   return(
     <>
-       <List />  
+       <List items = {listItems} />  
     </>
   );
    
--- src/List.jsx
+++ src/List.jsx
@@ -1,18 +1,12 @@
 
-function List(){
+function List(props){
     
-    const fruits = [{id : 1, name: "apple",   calories: 120},
-                    {id : 2, name: "pear",    calories: 23},
-                    {id : 3, name: "banana",  calories: 430},
-                    {id : 4, name: "coconut", calories: 100},
-                    {id : 5, name: "orange",  calories: 59},
-                ];      
-    fruits.sort((a, b) => a.name.localeCompare(b.name))                    
+  
+    const itemList = props.items;               
 
-    const listItem = fruits.map(fruit => <li key={fruit.id}>{fruit.name}:
-                                          &nbsp; <b>{fruit.calories}</b></li>);
+    const listItems = itemList.map(item => <li key={item.id}>{item.name}:
+                                          &nbsp; <b>{item.calories}</b></li>);
         
-        return(<ol>{listItem}</ol>);
+        return(<ol>{listItems}</ol>);
 }
-
 export default List
